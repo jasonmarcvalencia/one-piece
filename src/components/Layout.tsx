e8a8bba03ccf31367889ce7d9d4d2b8afe77fe5d
jasonmarcvalencia <jasonmarcvalencia19@gmail.com>
2026-03-14 21:51:09 +0800

    .

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import { ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <div className="min-h-screen flex flex-col bg-background">
-    <Navbar />
-    <main className="flex-1">{children}</main>
-    <Footer />
-  </div>
-);
+const Layout = ({ children }: { children: ReactNode }) => {
+  const isHome = useLocation().pathname === "/";
+  return (
+    <div className="min-h-screen flex flex-col bg-background">
+      <Navbar />
+      <main className={`flex-1 ${isHome ? "" : "pt-16"}`}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
